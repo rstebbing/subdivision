7255c6701f37a5a8622ebde59d1471b7a47405d4
Richard Stebbing <richie.stebbing@gmail.com>
2014-11-23 09:41:25 +0000

    Update doosabin/verification/evaluation.py

--- doosabin/verification/evaluation.py
+++ doosabin/verification/evaluation.py
@@ -1,69 +1,63 @@
 # evaluation.py
 
 # Imports
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from itertools import cycle
 
 import doosabin
 from common import example_extraordinary_patch
 
-# visualise_example_subdivision
-def visualise_example_subdivision(N, n):
-    X0 = example_extraordinary_patch(N)
-    A = doosabin.extended_subdivision_matrix(N)
-    A_ = doosabin.bigger_subdivision_matrix(N)
-    Xs = [X0]
-    Xs_ = []
-    for i in xrange(n):
+# main
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('N', nargs='?', type=int, default=7)
+    parser.add_argument('n', nargs='?', type=int, default=2)
+    parser.add_argument('m', nargs='?', type=int, default=21)
+    args = parser.parse_args()
+
+    # Generate example extraordinary patch with an extraordinary face of `N`
+    # sides.
+    X = example_extraordinary_patch(args.N)
+
+    # Visualise `n` "levels" of subdivision.
+    A = doosabin.extended_subdivision_matrix(args.N)
+    A_ = doosabin.bigger_subdivision_matrix(args.N)
+    Xs, Xs_ = [X], []
+    for i in xrange(args.n):
         Xi = Xs[-1]
         Xs.append(np.dot(A, Xi))
         Xs_.append(np.dot(A_, Xi))
 
     colours = cm.Set1(np.linspace(0.0, 1.0, 9, endpoint=True))[:, :3]
-    colour_iterable = cycle(colours)
     f, ax = plt.subplots()
     ax.set_aspect('equal')
+    ax.set_xticks([])
+    ax.set_yticks([])
     for i, Xi in enumerate(Xs):
         x, y = Xi.T
-        c = next(colour_iterable)
+        c = colours[i % colours.shape[0]]
         ax.plot(x, y, 'o-', c=c)
         if i >= 1:
             x1, y1 = Xs_[i - 1][-7:].T
             ax.plot(x1, y1, 'o-', c=c)
             ax.plot([x[-1], x1[0]], [y[-1], y1[0]], '--', c=c)
 
-    min_, max_ = np.amin(X0, axis=0), np.amax(X0, axis=0)
-    ax.set_xlim(min_[0] - 0.2, max_[0] + 0.2)
-    ax.set_ylim(min_[1] - 0.2, max_[1] + 0.2)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    return f, ax
-
-if __name__ == '__main__':
-    visualise_example_subdivision(7, 2)
-    plt.show()
-
-    # Check evaluation for an extraordinary patch.
-    N = 7
-    X = example_extraordinary_patch(N)
-    # x = 0.5 * (np.linspace(0.0, 5.0, X.size) % 1.0)
-    # X += x.reshape(X.shape)
-    t, step = np.linspace(0.0, 1.0, 11, endpoint=False, retstep=True)
+    # Evaluate points on the patch using recursive subdivision and the position
+    # basis functions.
+    t, step = np.linspace(0.0, 1.0, args.m, endpoint=False, retstep=True)
     t += 0.5 * step
     U = np.dstack(np.broadcast_arrays(t[:, np.newaxis], t)).reshape(-1, 2)
+    P = np.array([doosabin.recursive_evaluate(
+                    0, doosabin.biquadratic_bspline_position_basis,
+                    args.N, u, X)
+                  for u in U])
+    x, y = P.T
+    ax.plot(x, y, 'k.')
+    ax.set_title('N = %d, n = %d, # = %d' % (args.N, args.n, len(P)))
 
-    P = []
-    for u in U:
-        P.append(doosabin.recursive_evaluate(
-            doosabin.biquadratic_bspline_position_basis,
-            N, u, X, 0))
-
-    f, ax = plt.subplots()
-    ax.set_aspect('equal')
-    x, y = X.T
-    ax.plot(x, y, 'ro-')
-    x, y = np.transpose(P)
-    ax.plot(x, y, 'b.')
     plt.show()
+
+if __name__ == '__main__':
+    main()
