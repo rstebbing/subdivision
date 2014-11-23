8f08a82770274f0a07183fc4c76e559302f13db2
Richard Stebbing <richie.stebbing@gmail.com>
2014-11-23 17:33:50 +0000

    Split `sympy_polynomial_to_function` into common

--- loop/loop.py
+++ loop/loop.py
@@ -5,7 +5,10 @@ import numpy as np
 import re
 import sympy as sp
 from functools import partial
-from operator import add, attrgetter, mul
+from operator import add, mul
+
+# Requires common/python on `PYTHONPATH`.
+from sympy_ import sympy_polynomial_to_function
 
 # Loop Subdivision Matrices
 
@@ -197,36 +200,6 @@ def recursive_evaluate(p, b, N, u, X):
     for i in range(n - 1):
         x = np.dot(x, np.dot(A_, P3))
     return np.dot(x, np.dot(A_, X))
-
-# sympy_polynomial_to_function
-def sympy_polynomial_to_function(expr, symbs,
-                                 func_name='sympy_polynomial',
-                                 return_string_only=False):
-    poly = sp.poly(sp.collect(sp.expand(expr), symbs), *symbs)
-    input_names = map(attrgetter('name'), poly.gens)
-    num_names = len(input_names)
-
-    def term_string((powers, coeff)):
-        indices = filter(lambda i: powers[i] != 0,
-                         range(num_names))
-        if indices:
-            terms = map(lambda i: '(%s ** %s)' % (input_names[i],
-                                                  powers[i]),
-                        indices)
-            return '%s * %s' % (float(coeff), ' * '.join(terms))
-        else:
-            return '%s' % float(coeff)
-
-    poly_str = ' + '.join(map(term_string, poly.terms()))
-    function_str = 'def %s(%s):\n    return %s' % (
-        func_name, ', '.join(input_names), poly_str)
-
-    if return_string_only:
-        return function_str
-
-    exec_env = {}
-    exec function_str in exec_env
-    return exec_env[func_name]
 
 # sympy_exprs_to_function
 def sympy_exprs_to_function(exprs, func_name=None):
